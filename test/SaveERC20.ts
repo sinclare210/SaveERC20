282a2aaaa86a2224268b8753411779348d47caa3
Sinclair <sinclairolajuwon@gmail.com>
2024-09-11 18:36:27 -0500

    Not Done with testing

--- test/SaveERC20.ts
+++ test/SaveERC20.ts
@@ -6,7 +6,7 @@ import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import hre, { ethers } from "hardhat";
 
-describe("Lock", function () {
+describe("SINC", function () {
   // We define a fixture to reuse the same setup in every test.
   // We use loadFixture to run this setup once, snapshot that state,
   // and reset Hardhat Network to that snapshot in every test.
